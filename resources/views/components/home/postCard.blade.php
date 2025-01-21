05d2f9967ae9f15e1ceb18fd43d01dcdd79921f1
Erick Lima <ericklima63@gmail.com>
2025-01-20 21:20:16 -0300

    style: Improve post card edit and delete button styling for better user experience

--- resources/views/components/home/postCard.blade.php
+++ resources/views/components/home/postCard.blade.php
@@ -9,12 +9,14 @@
         <p>{{ $body }}</p>
     </div>
 
-    <div>
-        <a href="/edit-post/{{ $id }}">Edit</a>
+    <div class="grid grid-cols-[3fr,1fr] gap-4 items-center content-center">
+        <a href="/edit-post/{{ $id }}"
+            class="flex items-center h-full py-2 px-4 bg-black border-2 border-black text-white rounded-lg hover:bg-white hover:text-black  transition-colors duration-200 text-center font-medium shadow-md hover:shadow-lg">Edit</a>
 
         <form action="/delete-post/{{ $id }}" method="POST">
             @csrf
             @method('DELETE')
-            <button>Delete</button>
-    </div>
+            <button
+                class="w-full p-2 bg-red-500 text-white rounded-lg hover:bg-red-400 transition-colors duration-200 font-medium shadow-md hover:shadow-lg">Delete</button>
+        </form>
 </li>
